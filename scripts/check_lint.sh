39c8a3c5fb315dd99de71db17ab9c0e40873ad7d
Jae H. Yoo <40815393+jaeyoo@users.noreply.github.com>
2021-08-06 16:17:24 -0700

    Add __init__ & updated license header (#61)

--- scripts/check_lint.sh
+++ scripts/check_lint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2021 The QHBM Library Authors.
+# Copyright 2021 The QHBM Library Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
