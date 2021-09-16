98a7f7dec27f33e46276da2722954b6fea8090ed
zaqqwerty <antonio.martinez.8128@gmail.com>
2021-09-16 13:23:22 -0500

    update test

--- tests/vqt_test.py
+++ tests/vqt_test.py
@@ -86,15 +86,12 @@ class VQTTest(tf.test.TestCase):
 
     Since each qubit is independent, the loss is the sum over the individual
     qubit losses, and the gradients are the the per-qubit gradients.
-    """
-    bare_vqt = vqt.vqt
-
-    @tf.function
-    def tf_func_vqt(qhbm, num_samples, hamiltonian, beta):
-      """Simple wrapper to test tf.function tracing."""
-      return vqt.vqt(qhbm, num_samples, hamiltonian, beta)
-
-    for vqt_func in [bare_vqt, tf_func_vqt]:
+    """    
+    for vqt_func in [
+        vqt.vqt,
+        tf.function(vqt.vqt, experimental_compile=False),
+#        tf.function(vqt.vqt, experimental_compile=True)
+    ]:
       seed = None
       for num_qubits in [1, 2, 3, 4, 5]:
         # EBM
