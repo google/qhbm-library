362262885ae2d4804b9906f31b4611f2b9f9970c
zaqqwerty <antonio.martinez.8128@gmail.com>
2021-12-06 16:43:01 -0600

    circuit infer tests

--- qhbmlib/circuit_infer.py
+++ qhbmlib/circuit_infer.py
@@ -14,19 +14,54 @@
 # ==============================================================================
 """Tools for inference on quantum circuits."""
 
+import cirq
 import tensorflow as tf
 import tensorflow_quantum as tfq
 
+from qhbmlib import circuit_model
+
+
+class BitstringInjector(tf.keras.Model):
+  """Class for combining bitstrings and quantum circuits."""
+
+  def __init__(self, qubits, name="bit_circuit"):
+    """Initializes a BitstringInjector."""
+    super().__init__(name=name)
+    self.qubits = qubits
+    circuit = cirq.Circuit()
+    symbols = []
+    for n, q in enumerate(qubits):
+      bit = sympy.Symbol("{0}_bit_{1}".format(name, n))
+      circuit += cirq.X(q)**bit
+      symbols.append(bit)
+    self.bit_circuit = tfq.convert_to_tensor([circuit])
+    self.bit_symbols = tf.constant([str(x) for x in symbols])
+
+  def inject_bitstrings(self, bitstrings, qnn: circuit_model.QuantumCircuit):
+    """Returns circuits which are the qnn appended to bitstring injections."""
+    num_bitstrings = tf.shape(bitstrings)[0]
+    bit_circuits = tfq.resolve_parameters(
+        tf.tile(self.bit_circuit, [num_bitstrings]), self.bit_symbols,
+        tf.cast(bitstrings, tf.float32))
+    pqcs = tf.tile(qnn.pqc, [num_bitstrings])
+    return tfq.append_circuit(bit_circuits, pqcs)
+
+  def call(self, bitstrings, qnn):
+    return self.inject_bitstrings(bitstrings, qnn)
+
+  
 class Expectation(tf.keras.Model):
   """Class for taking expectation values of quantum circuits."""
   
   def __init__(self,
+               qubits: List[cirq.GridQubit],
                backend="noiseless",
                differentiator=None,
                name=None):
     """Initialize an instance of Expectation.
 
     Args:
+      qubits: The qubits on which this class makes inferences.
       backend: Optional Python `object` that specifies what backend TFQ will use
         to compute expectation values. Options are {"noisy", "noiseless"}.
         Users may also specify a preconfigured cirq execution
@@ -37,6 +72,9 @@ class Expectation(tf.keras.Model):
     """
     super().__init__(name=name)
 
+    self._qubits = qubits
+    self._bitstring_injector = BitstringInjector(qubits, name)
+    
     self._differentiator = differentiator
     if backend == "noiseless" or backend is None:
       self._backend = "noiseless"
@@ -64,6 +102,10 @@ class Expectation(tf.keras.Model):
     self._expectation_function = _expectation_function
 
   @property
+  def qubits(self):
+    return self._qubits
+
+  @property
   def backend(self):
     return self._backend
 
@@ -72,33 +114,26 @@ class Expectation(tf.keras.Model):
     return self._differentiator
 
   def expectation(self,
-                  counts,
-                  circuits,
-                  symbol_names,
-                  symbol_values,
-                  operators,
-                  reduce=True):
+                  qnn: circuit_model.QuantumCircuit,
+                  initial_states: tf.Tensor,
+                  counts: tf.Tensor,
+                  operators: tf.Tensor,
+                  reduce: bool=True):
     """Returns the expectation values of the operators against the QNN.
 
       Args:
-        counts: 1D tensor of dtype `tf.int32` such that `counts[i]` is the
-          relative weight of `circuits[i]` when computing expectations.
-          Additionally, if `self.backend != "noiseless", `counts[i]` samples
-          are drawn from `circuits[i]` and used to compute each expectation.
-        circuits: `tf.Tensor` of strings with shape [batch_size] containing
-            the string representations of the circuits to be executed.
-        symbol_names: `tf.Tensor` of strings with shape [n_params], which
-            is used to specify the order in which the values in
-            `symbol_values` should be placed inside of the circuits in
-            `programs`.
-        symbol_values: `tf.Tensor` of real numbers with shape
-            [batch_size, n_params] specifying parameter values to resolve
-            into the circuits specified by programs, following the ordering
-            dictated by `symbol_names`.
+        qnn: Unitary to feed forward.
         operators: `tf.Tensor` of strings with shape [n_ops], result of calling
           `tfq.convert_to_tensor` on a list of cirq.PauliSum, `[op1, op2, ...]`.
-          Will be tiled to measure `<op_j>_self.u_dagger|circuits[i]>`
+          Will be tiled to measure `<op_j>_(qnn)|initial_states[i]>`
           for each i and j.
+        initial_states: Shape [batch_size, num_qubits] of dtype `tf.int8`.
+          These are the initial states of each qubit in the circuit.
+        counts: Shape [batch_size] of dtype `tf.int32` such that `counts[i]` is
+          the weight of `initial_states[i]` when computing expectations.
+          Additionally, if `self.backend != "noiseless", `counts[i]` samples
+          are drawn from `(qnn)|initial_states[i]>` and used to compute
+          the the corresponding expectation.
         reduce: bool flag for whether or not to average over i.
 
       Returns:
@@ -107,6 +142,9 @@ class Expectation(tf.keras.Model):
         Else, a `tf.Tensor` with shape [batch_size, n_ops] whose entries are the
         unaveraged expectation values of each `operator` against each `circuit`.
       """
+    circuits = self._bitstring_injector(initial_states, qnn)
+    symbol_names = qnn.symbols
+    symbol_values = qnn.values
     num_circuits = tf.shape(circuits)[0]
     num_operators = tf.shape(operators)[0]
     tiled_values = tf.tile(tf.expand_dims(symbol_values, 0), [num_circuits, 1])
@@ -124,17 +162,16 @@ class Expectation(tf.keras.Model):
       return tf.reduce_sum(tf.transpose(probs * tf.transpose(expectations)), 0)
     return expectations
 
-
   def call(self,
+           qnn,
+           initial_states,
            counts,
-           circuits,
-           symbol_names,
-           symbol_values,
            operators,
            reduce=True):
-    return expectation(counts,
-                       circuits,
-                       symbol_names,
-                       symbol_values,
+    return expectation(qnn,
+                       initial_states,
+                       counts,
                        operators,
                        reduce=reduce)
+
+
---
+++ tests/circuit_infer_test.py
@@ -0,0 +1,236 @@
+# Copyright 2021 The QHBM Library Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for the circuit_infer module."""
+
+import random
+import itertools
+from absl import logging
+
+import cirq
+import math
+import numpy as np
+import sympy
+import tensorflow as tf
+import tensorflow_probability as tfp
+import tensorflow_quantum as tfq
+
+from qhbmlib import circuit_infer
+from tests import test_util
+
+# Global tolerance, set for float32.
+ATOL = 1e-5
+GRAD_ATOL = 2e-4
+
+
+def _pystr(x):
+  return [str(y) for y in x]
+
+
+class BitstringInjectorTest(tf.test.TestCase):
+  """Tests BitstringInjector."""
+
+  def test_init(self):
+    """Confirm correct bit injector circuit creation."""
+    expected_qubits = [
+        cirq.GridQubit(0, 2),
+        cirq.GridQubit(1, 4),
+        cirq.GridQubit(2, 2)
+    ]
+    expected_name = "build_bit_test"
+    actual_bit_injector = circuit_infer.BitstringInjector(expected_qubits, expected_name)
+    expected_symbols = list(
+        sympy.symbols(
+            "build_bit_test_bit_0 build_bit_test_bit_1 build_bit_test_bit_2"))
+    expected_circuit = tfq.convert_to_tensor([cirq.Circuit(
+        [cirq.X(q)**s for q, s in zip(my_qubits, expected_symbols)])])
+    self.assertAllEqual(actual_bit_injector.qubits, expected_qubits)
+    self.assertAllEqual(_pystr(actual_bit_injector.bit_symbols), _pystr(expected_symbols))
+    self.assertEqual(tfq.from_tensor(actual_bit_injector.bit_circuit), tfq.from_tensor(expected_circuit))
+
+  def test_inject_bitstrings(self):
+    """Confirms correct combination of bits and circuits."""
+    num_qubits = 5
+    qubits = cirq.GridQubit.rect(1, num_qubits)
+    actual_bit_injector = circuit_infer.BitstringInjector(qubits)
+
+    test_pqcs, _ = tfq.util.random_circuit_resolver_batch(qubits, 1)
+    test_qnn = circuit_model.DirectQuantumCircuit(test_pqcs[0])
+
+    bitstrings = 2 * list(itertools.product([0, 1], repeat=num_qubits))
+    bit_injectors = []
+    for b in bitstrings:
+      bit_injectors.append(
+          cirq.Circuit(cirq.X(q)**b_i for q, b_i in zip(qubits, b)))
+    expected_circuits = tfq.convert_to_tensor([b + test_pqcs[0] for b in bit_injectors])
+    self.assertAllEqual(tfq.from_tensor(actual_circuits), tfq.from_tensor(expected_circuits))
+
+
+class ExpectationTest(tf.test.TestCase):
+  """Tests the Expectation class."""
+
+  def test_init(self):
+    """Confirms Expectation is initialized correctly."""
+    expected_backend = "noiseless"
+    expected_differentiator = None
+    expected_name = "TestOE"
+    actual_exp = circuit_infer.Expectation(
+        backend=self.backend,
+        differentiator=self.differentiator,
+        name=self.name)
+    self.assertEqual(actual_exp.name, expected_name)
+    self.assertEqual(actual_exp.backend, expected_backend)
+    self.assertEqual(actual_exp.differentiation, expected_differentiator)
+
+  def test_expectation(self):
+    """Confirms basic correct expectation values and derivatives.
+
+    Consider a circuit where each qubit has a gate X^p. Diagonalization of X is
+        |0 1|   |-1 1||-1 0||-1/2 1/2|
+    X = |1 0| = | 1 1|| 0 1|| 1/2 1/2|
+    Therefore we have
+          |-1 1||(-1)^p 0||-1/2 1/2|       |(-1)^p  -(-1)^p|       |1 1|
+    X^p = | 1 1||   0   1|| 1/2 1/2| = 1/2 |-(-1)^p  (-1)^p| + 1/2 |1 1|
+    where (-1)^p = cos(pi * p) + isin(pi * p).
+    Consider the action on the two basis states:
+                 | (-1)^p|       |1|
+    X^p|0> = 1/2 |-(-1)^p| + 1/2 |1|
+                 |-(-1)^p|       |1|
+    X^p|1> = 1/2 | (-1)^p| + 1/2 |1|
+    so, for s in {0, 1},
+                 | ((-1)^s)(-1)^p|       |1|
+    X^p|s> = 1/2 |-((-1)^s)(-1)^p| + 1/2 |1|
+    similarly,
+    <0|(X^p)^dagger = 1/2 |((-1)^p)^dagger -((-1)^p)^dagger| + 1/2 |1 1|
+    <1|(X^p)^dagger = 1/2 |-((-1)^p)^dagger ((-1)^p)^dagger| + 1/2 |1 1|
+    so
+    <s|(X^p)^dagger = 1/2 |((-1)^s)((-1)^p)^dagger -((-1)^s)((-1)^p)^dagger|
+                                                                     + 1/2 |1 1|
+    where ((-1)^p)^dagger = cos(pi * p) - isin(pi * p).
+
+    We want to see what the expectation values <s|(X^p)^dagger W X^p|s> are,
+    for W in {X, Y, Z}.  Applying the above results, we have
+    <s|(X^p)^dagger X X^p|s> = 0
+    <s|(X^p)^dagger Y X^p|s> = -(-1)^s sin(pi * p)
+    <s|(X^p)^dagger Z X^p|s> = (-1)^s cos(pi * p)
+
+    Since these expectation values are in terms of p, we can calculate their
+    derivatives with respect to p:
+    d/dp <s|(X^p)^dagger X X^p|s> = 0
+    d/dp <s|(X^p)^dagger Y X^p|s> = -(-1)^s pi cos(pi * p)
+    d/dp <s|(X^p)^dagger Z X^p|s> = -(-1)^s pi sin(pi * p)
+    """
+    num_qubits = 5
+    qubits = cirq.GridQubit.rect(1, num_qubits)
+    exp_infer = circuit_infer.Expectation(qubits)
+    
+    # Build QNN representing X^p|s>
+    p_param = sympy.Symbol("p")
+    p_circuit = cirq.Circuit(cirq.X(q)**p_param for q in qubits)
+    p_qnn = circuit_model.DirectQuantumCircuit(
+        p_circuit,
+        initializer=tf.keras.initializers.RandomUniform(
+            minval=-5.0, maxval=5.0),
+        name="p_qnn")
+
+    # Choose some bitstrings.
+    num_bitstrings = 10
+    bitstrings_raw = []
+    for _ in range(num_bitstrings):
+      bitstrings_raw.append([])
+      for _ in range(self.num_qubits):
+        if random.choice([True, False]):
+          bitstrings_raw[-1].append(0)
+        else:
+          bitstrings_raw[-1].append(1)
+    bitstrings = tf.constant(bitstrings_raw, dtype=tf.int8)
+    counts = tf.random.uniform(
+        shape=[num_bitstrings], minval=1, maxval=1000, dtype=tf.int32)
+
+    # Get true expectation values based on the bitstrings.
+    expected_x_exps = []
+    expected_x_exps_grad = []
+    expected_y_exps = []
+    expected_y_exps_grad = []
+    expected_z_exps = []
+    expected_z_exps_grad = []
+    expected = [
+      expected_x_exps,
+      expected_x_exps_grad,
+      expected_y_exps,
+      expected_y_exps_grad,
+      expected_z_exps,
+      expected_z_exps_grad
+    ]
+    sin_pi_p = math.sin(math.pi * p_qnn.values[0])
+    cos_pi_p = math.cos(math.pi * p_qnn.values[0])
+    for bits in bitstrings_raw:
+      for exps in expected:
+        exps.append([])
+      for s in bits:
+        x_exps_true[-1].append(0)
+        x_exps_grad_true[-1].append(0)
+        y_exps_true[-1].append(-((-1.0)**s) * sin_pi_p)
+        y_exps_grad_true[-1].append(-((-1.0)**s) * math.pi * cos_pi_p)
+        z_exps_true[-1].append(((-1.0)**s) * cos_pi_p)
+        z_exps_grad_true[-1].append(-((-1.0)**s) * math.pi * sin_pi_p)
+    e_counts = tf.cast(tf.expand_dims(counts, 1), tf.float32)
+    total_counts = tf.cast(tf.reduce_sum(counts), tf.float32)
+    expected_reduced = []
+    for exps in expected:
+      expected_reduced.append(tf.reduce_sum(exps * e_counts, 0) / total_counts)
+
+    # Measure operators on every qubit.
+    x_ops = tfq.convert_to_tensor([1 * cirq.X(q) for q in self.raw_qubits])
+    y_ops = tfq.convert_to_tensor([1 * cirq.Y(q) for q in self.raw_qubits])
+    z_ops = tfq.convert_to_tensor([1 * cirq.Z(q) for q in self.raw_qubits])
+
+    # Check with reduce True (this is the default)
+    with tf.GradientTape(persistent=True) as tape:
+      x_exps_test = exp_infer.expectation(p_qnn, bitstrings, counts, x_ops)
+      y_exps_test = exp_infer.expectation(p_qnn, bitstrings, counts, y_ops)
+      z_exps_test = exp_infer.expectation(p_qnn, bitstrings, counts, z_ops)
+    x_exps_grad_test = tf.squeeze(tape.jacobian(x_exps_test, p_qnn.values))
+    y_exps_grad_test = tf.squeeze(tape.jacobian(y_exps_test, p_qnn.values))
+    z_exps_grad_test = tf.squeeze(tape.jacobian(z_exps_test, p_qnn.values))
+    del (tape)
+    self.assertAllClose(x_exps_test, x_exps_true_reduced, atol=ATOL)
+    self.assertAllClose(
+        x_exps_grad_test, x_exps_grad_true_reduced, atol=GRAD_ATOL)
+    self.assertAllClose(y_exps_test, y_exps_true_reduced, atol=ATOL)
+    self.assertAllClose(
+        y_exps_grad_test, y_exps_grad_true_reduced, atol=GRAD_ATOL)
+    self.assertAllClose(z_exps_test, z_exps_true_reduced, atol=ATOL)
+    self.assertAllClose(
+        z_exps_grad_test, z_exps_grad_true_reduced, atol=GRAD_ATOL)
+
+    # Check with reduce False
+    with tf.GradientTape(persistent=True) as tape:
+      x_exps_test = exp_infer.expectation(p_qnn, bitstrings, counts, x_ops, reduce=False)
+      y_exps_test = exp_infer.expectation(p_qnn, bitstrings, counts, y_ops, reduce=False)
+      z_exps_test = exp_infer.expectation(p_qnn, bitstrings, counts, z_ops, reduce=False)
+    x_exps_grad_test = tf.squeeze(tape.jacobian(x_exps_test, p_qnn.values))
+    y_exps_grad_test = tf.squeeze(tape.jacobian(y_exps_test, p_qnn.values))
+    z_exps_grad_test = tf.squeeze(tape.jacobian(z_exps_test, p_qnn.values))
+    self.assertAllClose(x_exps_test, x_exps_true, atol=ATOL)
+    self.assertAllClose(x_exps_test, x_exps_grad_true, atol=GRAD_ATOL)
+    self.assertAllClose(y_exps_test, y_exps_true, atol=ATOL)
+    self.assertAllClose(y_exps_grad_test, y_exps_grad_true, atol=GRAD_ATOL)
+    self.assertAllClose(z_exps_test, z_exps_true, atol=ATOL)
+    self.assertAllClose(z_exps_grad_test, z_exps_grad_true, atol=GRAD_ATOL)
+
+
+if __name__ == "__main__":
+  logging.info("Running circuit_infer_test.py ...")
+  tf.test.main()
