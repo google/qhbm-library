295f4c99888d8a99ac30e739e6ec667887c2614c
zaqqwerty <antonio.martinez.8128@gmail.com>
2022-01-04 20:49:47 -0600

    new fidelity calculator

--- qhbmlib/hamiltonian_infer.py
+++ qhbmlib/hamiltonian_infer.py
@@ -125,7 +125,7 @@ class QHBM(tf.keras.layers.Layer):
 def density_matrix(model: hamiltonian_model.Hamiltonian):
   e_inf = energy_infer.AnalyticEnergyInference(model.energy.num_bits)
   e_inf.infer(model.energy)
-  probabilities = tf.cast(e_inf.all_probabilities(), tf.complex64)
+  probabilities = tf.cast(e_inf.all_probabilities, tf.complex64)
   resolved_pqc = tfq.resolve_parameters(
       model.circuit.pqc,
       model.circuit.symbol_names,
@@ -136,3 +136,26 @@ def density_matrix(model: hamiltonian_model.Hamiltonian):
     tf.tile(
       tf.expand_dims(probabilities, 0), [tf.shape(unitary_matrix)[0], 1]))
   return tf.matmul(unitary_probs, tf.linalg.adjoint(unitary_matrix))
+
+
+def fidelity(model: hamiltonian_model.Hamiltonian, sigma: tf.Tensor):
+  e_inf = energy_infer.AnalyticEnergyInference(model.energy.num_bits)
+  e_inf.infer(model.energy)
+  e_rho = tf.cast(e_inf.all_probabilities(), tf.complex128)
+  resolved_pqc = tfq.resolve_parameters(
+      model.circuit.pqc,
+      model.circuit.symbol_names,
+      tf.expand_dims(model.circuit.symbol_values, 0))
+  unitary_matrix = tfq.layers.Unitary()(resolved_pqc).to_tensor()[0]
+  v_rho = tf.cast(unitary_matrix, tf.complex128)
+  sqrt_e_rho = tf.sqrt(e_rho)
+  v_rho_sqrt_e_rho = tf.multiply(
+      v_rho, tf.tile(tf.expand_dims(sqrt_e_rho, 0), (tf.shape(v_rho)[0], 1)))
+  rho_sqrt = tf.linalg.matmul(v_rho_sqrt_e_rho, tf.linalg.adjoint(v_rho))
+  omega = tf.linalg.matmul(
+      tf.linalg.matmul(rho_sqrt, tf.cast(sigma, tf.complex128)), rho_sqrt)
+  # TODO(zaqqwerty): find convincing proof that omega is hermitian,
+  # in order to go back to eigvalsh.
+  e_omega = tf.linalg.eigvals(omega)
+  return tf.cast(
+      tf.math.abs(tf.math.reduce_sum(tf.math.sqrt(e_omega)))**2, tf.float32)
