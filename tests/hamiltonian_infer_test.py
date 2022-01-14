1996ab37e1bcfb16ed90afb8d0b056bb54c3f4d4
zaqqwerty <antonio.martinez.8128@gmail.com>
2022-01-13 18:58:28 -0600

    remove extra features for now

--- tests/hamiltonian_infer_test.py
+++ tests/hamiltonian_infer_test.py
@@ -108,68 +108,6 @@ class QHBMTest(tf.test.TestCase):
     self.assertAllClose(
         actual_counts[0], actual_counts[1], atol=num_samples / 1000)
 
-  def test_expectation_cirq(self):
-    """Compares library expectation values to those from Cirq."""
-    # observable
-    num_bits = 3
-    num_ops = 4
-    qubits = cirq.GridQubit.rect(1, num_bits)
-    raw_ops = [test_util.get_random_pauli_sum(qubits) for _ in range(num_ops)]
-    for op in raw_ops:
-      print(op)
-    ops = tfq.convert_to_tensor(raw_ops)
-
-    # unitary
-    symbols = set()
-    num_symbols = 10
-    for _ in range(num_symbols):
-      symbols.add("".join(random.sample(string.ascii_letters, 10)))
-    symbols = list(symbols)
-    circuits, resolvers = tfq.util.random_symbol_circuit_resolver_batch(
-        qubits, symbols, 1)
-    resolver = {k: resolvers[0].value_of(k) for k in resolvers[0]}
-
-    # hamiltonian model and inference
-    energy = energy_model.BernoulliEnergy(list(range(num_bits)))
-    energy.build([None, num_bits])
-    circuit = circuit_model.QuantumCircuit(
-        circuits[0], tf.constant(symbols), [tf.Variable([resolver[s] for s in symbols])], [[]])
-    circuit.build([])
-    actual_hamiltonian = hamiltonian_model.Hamiltonian(energy, circuit)
-    e_infer = energy_infer.BernoulliEnergyInference()
-    e_infer.infer(energy)
-    q_infer = circuit_infer.QuantumInference()
-    actual_h_infer = hamiltonian_infer.QHBM(e_infer, q_infer)
-
-    # sample bitstrings
-    num_samples = 1e7
-    samples = e_infer.sample(num_samples)
-    bitstrings, counts = util.unique_bitstrings_with_counts(samples)
-    bit_list = bitstrings.numpy().tolist()
-    counts_list = counts.numpy().tolist()
-    num_unique = tf.shape(bitstrings)[0]
-
-    # bitstring injectors
-    bitstring_circuit = circuit_model_utils.bit_circuit(qubits)
-    bitstring_symbols = tfq.util.get_circuit_symbols(bitstring_circuit)
-    bitstring_resolvers = [{s: b for s, b in zip(bitstring_symbols, bstr)} for bstr in bit_list]
-    total_resolvers = [{**r, **resolver} for r in bitstring_resolvers]
-    print(total_resolvers)
-    total_circuit = bitstring_circuit + circuits[0]
-    tiled_total_circuit = [total_circuit] * num_unique.numpy().tolist()
-    raw_expectation_list = [
-        [cirq.Simulator().simulate_expectation_values(c, o, r)[0] for o in raw_ops]
-    for c, r in zip(tiled_total_circuit, total_resolvers)]
-    expected_expectations = utils.weighted_average(raw_expectation_list, counts)
-
-    actual_expectations = actual_h_infer.expectation(
-        actual_hamiltonian, ops, num_samples)
-    self.assertAllClose(actual_expectations, expected_expectations)
-
-  def test_sample(self):
-    """Compares library samples to those from Cirq."""
-    pass
-
 
 if __name__ == "__main__":
   absl.logging.info("Running hamiltonian_infer_test.py ...")
