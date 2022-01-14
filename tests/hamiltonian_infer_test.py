7d58bec87a5b0e0684b5b14bcd7933ed20b88ca4
zaqqwerty <antonio.martinez.8128@gmail.com>
2022-01-13 18:00:15 -0600

    expectation test

--- tests/hamiltonian_infer_test.py
+++ tests/hamiltonian_infer_test.py
@@ -25,10 +25,12 @@ from tensorflow_quantum.python import util as tfq_util
 
 from qhbmlib import circuit_infer
 from qhbmlib import circuit_model
+from qhbmlib import circuit_model_utils
 from qhbmlib import energy_infer
 from qhbmlib import energy_model
 from qhbmlib import hamiltonian_model
 from qhbmlib import hamiltonian_infer
+from qhbmlib import util
 from tests import test_util
 
 
@@ -108,11 +110,16 @@ class QHBMTest(tf.test.TestCase):
 
   def test_expectation_cirq(self):
     """Compares library expectation values to those from Cirq."""
+    # observable
     num_bits = 3
+    num_ops = 4
     qubits = cirq.GridQubit.rect(1, num_bits)
-    raw_op = test_util.get_random_pauli_sum(qubits)
-    ops = tfq.convert_to_tensor([raw_op])
+    raw_ops = [test_util.get_random_pauli_sum(qubits) for _ in range(num_ops)]
+    for op in raw_ops:
+      print(op)
+    ops = tfq.convert_to_tensor(raw_ops)
 
+    # unitary
     symbols = set()
     num_symbols = 10
     for _ in range(num_symbols):
@@ -120,27 +127,44 @@ class QHBMTest(tf.test.TestCase):
     symbols = list(symbols)
     circuits, resolvers = tfq.util.random_symbol_circuit_resolver_batch(
         qubits, symbols, 1)
+    resolver = {k: resolvers[0].value_of(k) for k in resolvers[0]}
 
+    # hamiltonian model and inference
     energy = energy_model.BernoulliEnergy(list(range(num_bits)))
     energy.build([None, num_bits])
     circuit = circuit_model.QuantumCircuit(
-        circuits[0], tf.constant(symbols), [tf.Variable([resolvers[0][s] for s in symbols])], [[]])
+        circuits[0], tf.constant(symbols), [tf.Variable([resolver[s] for s in symbols])], [[]])
     circuit.build([])
     actual_hamiltonian = hamiltonian_model.Hamiltonian(energy, circuit)
-
     e_infer = energy_infer.BernoulliEnergyInference()
+    e_infer.infer(energy)
     q_infer = circuit_infer.QuantumInference()
     actual_h_infer = hamiltonian_infer.QHBM(e_infer, q_infer)
 
+    # sample bitstrings
     num_samples = 1e7
-    actual_expectation = actual_h_infer.expectation(
-        actual_hamiltonian, ops, num_samples)
+    samples = e_infer.sample(num_samples)
+    bitstrings, counts = util.unique_bitstrings_with_counts(samples)
+    bit_list = bitstrings.numpy().tolist()
+    counts_list = counts.numpy().tolist()
+    num_unique = tf.shape(bitstrings)[0]
 
-    total_circuits = 
-    expected_expectation = cirq.Simulator().simulate_expectation_values(
-        circuits[0], [raw_op], resolvers[0])
-    
-    self.assertAllClose(actual_expectation, expected_expectation)
+    # bitstring injectors
+    bitstring_circuit = circuit_model_utils.bit_circuit(qubits)
+    bitstring_symbols = tfq.util.get_circuit_symbols(bitstring_circuit)
+    bitstring_resolvers = [{s: b for s, b in zip(bitstring_symbols, bstr)} for bstr in bit_list]
+    total_resolvers = [{**r, **resolver} for r in bitstring_resolvers]
+    print(total_resolvers)
+    total_circuit = bitstring_circuit + circuits[0]
+    tiled_total_circuit = [total_circuit] * num_unique.numpy().tolist()
+    raw_expectation_list = [
+        [cirq.Simulator().simulate_expectation_values(c, o, r)[0] for o in raw_ops]
+    for c, r in zip(tiled_total_circuit, total_resolvers)]
+    expected_expectations = utils.weighted_average(raw_expectation_list, counts)
+
+    actual_expectations = actual_h_infer.expectation(
+        actual_hamiltonian, ops, num_samples)
+    self.assertAllClose(actual_expectations, expected_expectations)
 
   def test_sample(self):
     """Compares library samples to those from Cirq."""
