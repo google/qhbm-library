d1bef66915bfd1af29f6df182bf224b4ea74bc9c
zaqqwerty <antonio.martinez.8128@gmail.com>
2022-02-03 18:08:59 -0600

    temp add hypernetwork test

--- tests/vqt_loss_test.py
+++ tests/vqt_loss_test.py
@@ -34,6 +34,7 @@ RTOL = 3e-2
 class VQTTest(tf.test.TestCase):
   """Tests for VQT."""
 
+  @test_util.eager_mode_toggle
   def test_loss_value_x_rot(self):
     """Confirms correct values for a single qubit X rotation with H=Y.
 
@@ -45,9 +46,11 @@ class VQTTest(tf.test.TestCase):
     qubit losses, and the gradients are the the per-qubit gradients.
     """
 
+    @tf.function
     def vqt_wrapper(qhbm_infer, model, num_samples, hamiltonian, beta):
       return vqt_loss.vqt(qhbm_infer, model, num_samples, hamiltonian, beta)
-    for num_qubits in [2, 3, 4, 5]:
+
+    for num_qubits in [1, 2, 3, 4, 5]:
       # model definition
       tf_random_seed = None
       ebm_init = tf.keras.initializers.RandomUniform(
@@ -105,90 +108,87 @@ class VQTTest(tf.test.TestCase):
         test_beta * tf.math.sin(test_phis) + test_thetas)
       expected_phis_grads = test_beta * tf.math.tanh(
         test_thetas) * tf.math.cos(test_phis)
-      print(f"actual_phis_grads: {actual_phis_grads}")
-      print(f"test_thetas: {test_thetas}")
-      print(f"expected_thetas_grads: {expected_thetas_grads}")
-      print(f"actual_thetas_grads: {actual_thetas_grads}")
       self.assertAllClose(
         actual_thetas_grads, expected_thetas_grads, rtol=RTOL)
       self.assertAllClose(actual_phis_grads, expected_phis_grads, rtol=RTOL)
 
-  # def test_hypernetwork(self):
-  #   for num_qubits in [1, 2, 3, 4, 5]:
-  #     qubits = cirq.GridQubit.rect(1, num_qubits)
-  #     test_qhbm = test_util.get_random_qhbm(qubits, 1,
-  #                                           "VQTHyperTest{}".format(num_qubits))
-  #     ham = test_util.get_random_pauli_sum(qubits)
-  #     tf_ham = tfq.convert_to_tensor([ham])
-  #     trainable_variables_shapes = [
-  #         tf.shape(var) for var in test_qhbm.trainable_variables
-  #     ]
-  #     trainable_variables_sizes = [
-  #         tf.size(var) for var in test_qhbm.trainable_variables
-  #     ]
-  #     trainable_variables_size = tf.reduce_sum(
-  #         tf.stack(trainable_variables_sizes))
+  def test_hypernetwork(self):
+    for num_qubits in [1, 2, 3, 4, 5]:
+      qubits = cirq.GridQubit.rect(1, num_qubits)
+      energy  
+      model = test_util.get_random_qhbm(qubits, 1,
+                                            "VQTHyperTest{}".format(num_qubits))
+      ham = test_util.get_random_pauli_sum(qubits)
+      tf_ham = tfq.convert_to_tensor([ham])
+      trainable_variables_shapes = [
+          tf.shape(var) for var in model.trainable_variables
+      ]
+      trainable_variables_sizes = [
+          tf.size(var) for var in model.trainable_variables
+      ]
+      trainable_variables_size = tf.reduce_sum(
+          tf.stack(trainable_variables_sizes))
 
-  #     input_size = 15
-  #     hypernetwork = tf.keras.Sequential([
-  #         tf.keras.layers.Dense(15, 'relu', input_shape=(input_size,)),
-  #         tf.keras.layers.Dense(10, 'tanh', input_shape=(input_size,)),
-  #         tf.keras.layers.Dense(5, 'sigmoid', input_shape=(input_size,)),
-  #         tf.keras.layers.Dense(trainable_variables_size)
-  #     ])
-  #     input = tf.random.uniform([1, input_size])
+      input_size = 15
+      hypernetwork = tf.keras.Sequential([
+          tf.keras.layers.Dense(15, 'relu', input_shape=(input_size,)),
+          tf.keras.layers.Dense(10, 'tanh', input_shape=(input_size,)),
+          tf.keras.layers.Dense(5, 'sigmoid', input_shape=(input_size,)),
+          tf.keras.layers.Dense(trainable_variables_size)
+      ])
+      input = tf.random.uniform([1, input_size])
 
-  #     with tf.GradientTape() as tape:
-  #       output = tf.squeeze(hypernetwork(input))
-  #       index = 0
-  #       output_trainable_variables = []
-  #       for size, shape in zip(trainable_variables_sizes,
-  #                              trainable_variables_shapes):
-  #         output_trainable_variables.append(
-  #             tf.reshape(output[index:index + size], shape))
-  #         index += size
-  #       test_qhbm.trainable_variables = output_trainable_variables
-  #       loss = vqt.vqt(test_qhbm, tf.constant(int(5e6)), tf_ham,
-  #                      tf.constant(1.0))
-  #     grads = tape.gradient(loss, [
-  #         hypernetwork.trainable_variables, output,
-  #         test_qhbm.trainable_variables
-  #     ])
-  #     hyper_grads = grads[0]
-  #     output_grad = grads[1]
-  #     qhbm_grads = grads[2]
+      with tf.GradientTape() as tape:
+        output = tf.squeeze(hypernetwork(input))
+        index = 0
+        output_trainable_variables = []
+        for size, shape in zip(trainable_variables_sizes,
+                               trainable_variables_shapes):
+          output_trainable_variables.append(
+              tf.reshape(output[index:index + size], shape))
+          index += size
+        model.trainable_variables = output_trainable_variables
+        loss = vqt.vqt(test_qhbm, tf.constant(int(5e6)), tf_ham,
+                       tf.constant(1.0))
+      grads = tape.gradient(loss, [
+          hypernetwork.trainable_variables, output,
+          model.trainable_variables
+      ])
+      hyper_grads = grads[0]
+      output_grad = grads[1]
+      qhbm_grads = grads[2]
 
-  #     qhbm_grad_flat = []
-  #     for grad in qhbm_grads:
-  #       qhbm_grad_flat.append(tf.reshape(grad, [-1]))
-  #     qhbm_grad_flat = tf.concat(qhbm_grad_flat, 0)
-  #     self.assertAllEqual(qhbm_grad_flat, output_grad)
+      qhbm_grad_flat = []
+      for grad in qhbm_grads:
+        qhbm_grad_flat.append(tf.reshape(grad, [-1]))
+      qhbm_grad_flat = tf.concat(qhbm_grad_flat, 0)
+      self.assertAllEqual(qhbm_grad_flat, output_grad)
 
-  #     for grad in hyper_grads:
-  #       self.assertIsNotNone(grad)
+      for grad in hyper_grads:
+        self.assertIsNotNone(grad)
 
-  #     c = tf.Variable(tf.random.uniform([trainable_variables_size]))
-  #     input = tf.random.uniform([trainable_variables_size])
-  #     with tf.GradientTape() as tape:
-  #       output = c * input
-  #       index = 0
-  #       output_trainable_variables = []
-  #       for size, shape in zip(trainable_variables_sizes,
-  #                              trainable_variables_shapes):
-  #         output_trainable_variables.append(
-  #             tf.reshape(output[index:index + size], shape))
-  #         index += size
-  #       test_qhbm.trainable_variables = output_trainable_variables
-  #       loss = vqt.vqt(test_qhbm, tf.constant(int(5e6)), tf_ham,
-  #                      tf.constant(1.0))
-  #     grads = tape.gradient(loss, [c, test_qhbm.trainable_variables])
-  #     c_grad = grads[0]
-  #     qhbm_grads = grads[1]
-  #     qhbm_grad_flat = []
-  #     for grad in qhbm_grads:
-  #       qhbm_grad_flat.append(tf.reshape(grad, [-1]))
-  #     qhbm_grad_flat = tf.concat(qhbm_grad_flat, 0)
-  #     self.assertAllEqual(input * qhbm_grad_flat, c_grad)
+      c = tf.Variable(tf.random.uniform([trainable_variables_size]))
+      input = tf.random.uniform([trainable_variables_size])
+      with tf.GradientTape() as tape:
+        output = c * input
+        index = 0
+        output_trainable_variables = []
+        for size, shape in zip(trainable_variables_sizes,
+                               trainable_variables_shapes):
+          output_trainable_variables.append(
+              tf.reshape(output[index:index + size], shape))
+          index += size
+        model.trainable_variables = output_trainable_variables
+        loss = vqt.vqt(test_qhbm, tf.constant(int(5e6)), tf_ham,
+                       tf.constant(1.0))
+      grads = tape.gradient(loss, [c, model.trainable_variables])
+      c_grad = grads[0]
+      qhbm_grads = grads[1]
+      qhbm_grad_flat = []
+      for grad in qhbm_grads:
+        qhbm_grad_flat.append(tf.reshape(grad, [-1]))
+      qhbm_grad_flat = tf.concat(qhbm_grad_flat, 0)
+      self.assertAllEqual(input * qhbm_grad_flat, c_grad)
 
 
 if __name__ == "__main__":
