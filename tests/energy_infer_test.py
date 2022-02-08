0dddafbc88beb5b328c426512265ccd988816696
zaqqwerty <antonio.martinez.8128@gmail.com>
2022-02-08 04:57:01 -0600

    tests passing with three bit samples

--- tests/energy_infer_test.py
+++ tests/energy_infer_test.py
@@ -454,7 +454,7 @@ class AnalyticEnergyInferenceTest(tf.test.TestCase):
   def test_expectation_finite_difference(self):
     """Tests a function with nested structural output."""
 
-    num_bits = 2
+    num_bits = 3
     order = 2
     nonzero_init = tf.keras.initializers.RandomUniform(1, 2, seed=self.tf_random_seed)
     energy = energy_model.KOBE(list(range(num_bits)), order, nonzero_init)
@@ -465,8 +465,8 @@ class AnalyticEnergyInferenceTest(tf.test.TestCase):
     scalar_var = tf.Variable(tf.random.uniform([], 1, 2, seed=self.tf_random_seed))
     print(scalar_var)
 
-    num_units = 10
-    dense = tf.keras.layers.Dense(10, kernel_initializer=nonzero_init, bias_initializer=nonzero_init)
+    num_units = 5
+    dense = tf.keras.layers.Dense(num_units, kernel_initializer=nonzero_init, bias_initializer=nonzero_init)
     dense.build([None, num_bits])
 
     def f(bitstrings):
@@ -480,41 +480,47 @@ class AnalyticEnergyInferenceTest(tf.test.TestCase):
     e_infer = energy_infer.AnalyticEnergyInference(num_bits, seed=self.tfp_seed)
     e_infer.infer(energy)
 
-    num_samples = int(5e7)
+    num_samples = int(1e6)
     with tf.GradientTape() as tape:
       actual_expectation = e_infer.expectation(f, num_samples)
     actual_derivative = tape.gradient(actual_expectation, energy_var)
 
-    samples = e_infer.sample(num_samples)
-    bitstrings, counts = utils.unique_bitstrings_with_counts(samples)
-    values = f(bitstrings)
-    expected_expectation = tf.nest.map_structure(lambda x: utils.weighted_average(counts, x), values)
-    tf.nest.map_structure(lambda x: self.assertAllGreater(tf.abs(x), self.not_zero_atol), expected_expectation)
-    self.assertAllClose(actual_expectation, expected_expectation)
-
-    delta = 2e-3
     # Trainable variable of KOBE is 1D.
     num_elts = tf.size(energy_var)
-    derivative_list = []
-    for k in range(num_elts):
+
+    def delta_expectation(delta, k):
+      """Calculate the expectation with kth variable perturbed."""
       old_value = energy_var.read_value()
       energy_var.assign(old_value + delta * tf.one_hot(k, num_elts))
       e_infer.infer(energy)
       samples = e_infer.sample(num_samples)
       bitstrings, counts = utils.unique_bitstrings_with_counts(samples)
       values = f(bitstrings)
-      forward_expected_expectation = tf.nest.map_structure(lambda x: utils.weighted_average(counts, x), values)
-
-      energy_var.assign(old_value - delta * tf.one_hot(k, num_elts))
-      e_infer.infer(energy)
-      samples = e_infer.sample(num_samples)
-      bitstrings, counts = utils.unique_bitstrings_with_counts(samples)
-      values = f(bitstrings)
-      backward_expected_expectation = tf.nest.map_structure(lambda x: utils.weighted_average(counts, x), values)
-      expectation_difference = tf.nest.map_structure(tf.math.subtract, forward_expected_expectation, backward_expected_expectation)
-      expectation_difference_sum = tf.reduce_sum(tf.stack(tf.nest.map_structure(tf.reduce_sum, tf.nest.flatten(expectation_difference))))
-      derivative_list.append(expectation_difference_sum.numpy()/(2.0 * delta))
+      delta_expectation = tf.nest.map_structure(lambda x: utils.weighted_average(counts, x), values)
       energy_var.assign(old_value)
+      return delta_expectation
+
+    expected_expectation = delta_expectation(0, 0)
+    tf.nest.map_structure(lambda x: self.assertAllGreater(tf.abs(x), self.not_zero_atol), expected_expectation)
+    self.assertAllClose(actual_expectation, expected_expectation)
+
+    def approximate_derivative(delta, k):
+      """Returns a five point stencil approximation to the derivative."""
+      forward_twice = delta_expectation(2.0 * delta, k)
+      forward_once = delta_expectation(delta, k)
+      backward_once = delta_expectation(-1.0 * delta, k)
+      backward_twice = delta_expectation(-2.0 * delta, k)
+      numerator_flat = tf.nest.flatten(tf.nest.map_structure(lambda a, b, c, d: -1.0 * a + 8.0 * b - 8.0 * c + d, forward_twice, forward_once, backward_once, backward_twice))
+      numerator = tf.reduce_sum(tf.stack(tf.nest.map_structure(tf.reduce_sum, numerator_flat)))
+      return numerator / (12.0 * delta)
+
+    # Delta this large is ok since five point stencil has error ~delta ** 4
+    # See wikipedia page on "five point stencil"
+    delta = 2e-1
+    derivative_list = []
+
+    for k in range(num_elts):
+      derivative_list.append(approximate_derivative(delta, k).numpy())
     expected_derivative = tf.constant(derivative_list)
     tf.nest.map_structure(lambda x: self.assertAllGreater(tf.abs(x), self.not_zero_atol), expected_derivative)
     self.assertAllClose(actual_derivative, expected_derivative, rtol=self.grad_close_rtol)
