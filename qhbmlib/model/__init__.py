919bb7e105f91a1175ec50e2b79795c3b044c9cf
zaqqwerty <antonio.martinez.8128@gmail.com>
2022-02-19 17:55:06 -0600

    add model init

---
+++ qhbmlib/model/__init__.py
@@ -0,0 +1,22 @@
+# Copyright 2021 The QHBM Library Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Module for qhbmlib.model.*"""
+
+from qhbmlib.model.circuit import QuantumCircuit, DirectQuantumCircuit, QAIA
+from qhbmlib.model.circuit_utils import bit_circuit
+from qhbmlib.model.energy import BitstringEnergy, PauliMixin, BernoulliEnergy, KOBE
+from qhbmlib.model.energy_utils import SpinsFromBitstrings, VariableDot, Parity
+from qhbmlib.model.hamiltonian import Hamiltonian
+
