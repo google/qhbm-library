db3531ca55599c445e33e01015973de0d9956368
zaqqwerty <antonio.martinez.8128@gmail.com>
2022-02-19 20:25:34 -0600

    add init

---
+++ qhbmlib/infer/__init__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 The QHBM Library Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Module for qhbmlib.infer.*"""
+
+from qhbmlib.infer.ebm import EnergyInferenceBase, EnergyInference, AnalyticEnergyInference, BernoulliEnergyInference
+from qhbmlib.infer.ebm_utils import probabilities
+from qhbmlib.infer.qhbm import QHBM
+from qhbmlib.infer.qhbm_utils import density_matrix, fidelity
