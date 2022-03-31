62f30e49b48eb2d57f2fbb5900e8bcac530a6e7a
zaqqwerty <antonio.martinez.8128@gmail.com>
2022-03-31 17:45:43 -0500

    update sampled qnn

--- qhbmlib/inference/qnn.py
+++ qhbmlib/inference/qnn.py
@@ -132,158 +132,121 @@ class AnalyticQuantumInference(QuantumInference):
     return post_process(expectations)
 
 
-# class SampledQuantumInference(tf.keras.layers.Layer):
-#   """Sampling methods for inference on QuantumCircuit objects.
+class SampledQuantumInference(tf.keras.layers.Layer):
+  """Sampling methods for inference on QuantumCircuit objects.
 
-#   This class uses the TensorFlow Quantum `SampledExpectation` and `Sample`
-#   layers to compute expectation values of observables.  It uses parameter
-#   shift gradients to compute derivatives of those expectation values.
-#   """
+  This class uses the TensorFlow Quantum `SampledExpectation` and `Sample`
+  layers to compute expectation values of observables.  It uses parameter
+  shift gradients to compute derivatives of those expectation values.
+  """
 
-#   def __init__(self,
-#                input_circuit: circuit.QuantumCircuit,
-#                expectation_samples: int,
-#                name: Union[None, str] = None):
-#     """Initialize an SampledQuantumInference layer.
+  def __init__(self,
+               input_circuit: circuit.QuantumCircuit,
+               expectation_samples: int,
+               name: Union[None, str] = None):
+    """Initialize an SampledQuantumInference layer.
 
-#     Args:
-#       input_circuit: The parameterized quantum circuit on which to do inference.
-#       expectation_samples: Number of samples to use when estimating the
-#         expectation value of each observable on each input circuit.
-#       name: Identifier for this inference engine.
-#     """
-#     super().__init__(input_circuit, name)
-#     # Expand for compatibility with sample layer
-#     self._expectation_samples = tf.constant([expectation_samples],
-#                                             dtype=tf.int32)
-#     self._sample_layer = tfq.layers.Sample()
-#     self._expectation_layer = tfq.layers.SampledExpectation()
+    Args:
+      input_circuit: The parameterized quantum circuit on which to do inference.
+      expectation_samples: Number of samples to use when estimating the
+        expectation value of each observable on each input circuit.
+      name: Identifier for this inference engine.
+    """
+    super().__init__(input_circuit, name)
+    # Expand for compatibility with sample layer
+    self._expectation_samples = tf.constant([expectation_samples],
+                                            dtype=tf.int32)
+    self._sample_layer = tfq.layers.Sample()
+    self._expectation_layer = tfq.layers.SampledExpectation()
 
-#       def _expectation_function(circuits, symbol_names, symbol_values,
-#                                 operators, repetitions):
-#         return self._expectation_layer(
-#             circuits,
-#             symbol_names=symbol_names,
-#             symbol_values=symbol_values,
-#             operators=operators,
-#             repetitions=repetitions,
-#         )
+  def _sampled_expectation(self, circuits, symbol_names, symbol_values, observable):
+    @tf.custom_gradient
+    def _inner_expectation():
+      """Enables derivatives."""
+      num_circuits = tf.shape(circuits)[0]
+      unique_samples = self._sample_layer(
+          circuits,
+          symbol_names=symbol_names,
+          symbol_values=symbol_values,
+          repetitions=self._expectation_samples).to_tensor()
+      with tf.GradientTape() as thetas_tape:
+        unique_expectations = tf.map_fn(
+            lambda x: tf.math.reduce_mean(observable.energy(x)),
+            unique_samples,
+            fn_output_signature=tf.float32)
+        forward_pass = tf.expand_dims(unique_expectations, 1)
 
-#     self._expectation_function = _expectation_function
+      def grad_fn(*upstream, variables):
+        """Use `get_gradient_circuits` method to get QNN variable derivatives"""
+        # This block adapted from my `differentiate_sampled` in TFQ.
+        (batch_programs, new_symbol_names, batch_symbol_values, batch_weights,
+         batch_mapper) = self.differentiator.get_gradient_circuits(
+             circuits, symbol_names, symbol_values)
+        m_i = tf.shape(batch_programs)[1]
+        # shape is [num_circuits, m_i, n_ops]
+        n_batch_programs = tf.size(batch_programs)
+        n_symbols = tf.shape(new_symbol_names)[0]
+        gradient_samples = self._sample_layer(
+            tf.reshape(batch_programs, [n_batch_programs]),
+            symbol_names=new_symbol_names,
+            symbol_values=tf.reshape(batch_symbol_values,
+                                     [n_batch_programs, n_symbols]),
+            repetitions=self._expectation_samples).to_tensor()
+        gradient_expectations = tf.map_fn(
+            lambda x: tf.math.reduce_mean(observable.energy(x)),
+            gradient_samples,
+            fn_output_signature=tf.float32)
+        # last dimension is number of observables.
+        # TODO(#207): parameterize it if more than one observable is accepted.
+        batch_expectations = tf.reshape(gradient_expectations,
+                                        [num_circuits, m_i, 1])
 
-#   def _sampled_expectation(self, initial_states, observable):
-#     @tf.custom_gradient
-#     def _inner_expectation():
-#       """Enables derivatives."""
-#       unique_states, idx, _ = utils.unique_bitstrings_with_counts(
-#           initial_states)
-#       total_circuit = self.circuit + observable.circuit_dagger
-#       unique_circuits = total_circuit(unique_states)
-#       num_unique_circuits = tf.shape(unique_circuits)[0]
-#       unique_tiled_values = tf.tile(
-#           tf.expand_dims(total_circuit.symbol_values, 0),
-#           [num_unique_circuits, 1])
-#       unique_samples = self._sample_layer(
-#           unique_circuits,
-#           symbol_names=total_circuit.symbol_names,
-#           symbol_values=unique_tiled_values,
-#           repetitions=self._expectation_samples).to_tensor()
-#       with tf.GradientTape() as thetas_tape:
-#         unique_expectations = tf.map_fn(
-#             lambda x: tf.math.reduce_mean(observable.energy(x)),
-#             unique_samples,
-#             fn_output_signature=tf.float32)
-#         forward_pass = tf.expand_dims(unique_expectations, 1)
+        # In the einsum equation, s is the symbols index, m is the
+        # differentiator tiling index, o is the observables index.
+        # `batch_jacobian` has shape [num_unique_programs, n_symbols, n_ops]
+        unique_batch_jacobian = tf.map_fn(
+            lambda x: tf.einsum("sm,smo->so", x[0], tf.gather(
+                x[1], x[2], axis=0)),
+            (batch_weights, batch_expectations, batch_mapper),
+            fn_output_signature=tf.float32)
 
-#       def grad_fn(*upstream, variables):
-#         """Use `get_gradient_circuits` method to get QNN variable derivatives"""
-#         # This block adapted from my `differentiate_sampled` in TFQ.
-#         (batch_programs, new_symbol_names, batch_symbol_values, batch_weights,
-#          batch_mapper) = self.differentiator.get_gradient_circuits(
-#              unique_circuits, total_circuit.symbol_names, unique_tiled_values)
-#         m_i = tf.shape(batch_programs)[1]
-#         # shape is [num_unique_circuits, m_i, n_ops]
-#         n_batch_programs = tf.size(batch_programs)
-#         n_symbols = tf.shape(new_symbol_names)[0]
-#         gradient_samples = self._sample_layer(
-#             tf.reshape(batch_programs, [n_batch_programs]),
-#             symbol_names=new_symbol_names,
-#             symbol_values=tf.reshape(batch_symbol_values,
-#                                      [n_batch_programs, n_symbols]),
-#             repetitions=self._expectation_samples).to_tensor()
-#         gradient_expectations = tf.map_fn(
-#             lambda x: tf.math.reduce_mean(observable.energy(x)),
-#             gradient_samples,
-#             fn_output_signature=tf.float32)
-#         # last dimension is number of observables.
-#         # TODO(#207): parameterize it if more than one observable is accepted.
-#         batch_expectations = tf.reshape(gradient_expectations,
-#                                         [num_unique_circuits, m_i, 1])
+        # Connect upstream to symbol_values gradient
+        symbol_values_gradients = tf.einsum("pso,po->ps", unique_batch_jacobian,
+                                            upstream[0])
 
-#         # In the einsum equation, s is the symbols index, m is the
-#         # differentiator tiling index, o is the observables index.
-#         # `batch_jacobian` has shape [num_unique_programs, n_symbols, n_ops]
-#         unique_batch_jacobian = tf.map_fn(
-#             lambda x: tf.einsum("sm,smo->so", x[0], tf.gather(
-#                 x[1], x[2], axis=0)),
-#             (batch_weights, batch_expectations, batch_mapper),
-#             fn_output_signature=tf.float32)
-#         expanded_jacobian = utils.expand_unique_results(unique_batch_jacobian,
-#                                                         idx)
+        thetas_gradients = thetas_tape.gradient(
+            forward_pass,
+            variables,
+            output_gradients=upstream[0],
+            unconnected_gradients=tf.UnconnectedGradients.ZERO)
 
-#         # Connect upstream to symbol_values gradient
-#         symbol_values_gradients = tf.einsum("pso,po->ps", expanded_jacobian,
-#                                             upstream[0])
+        # Note: upstream gradient is already a coefficient below.
+        return (None, None, symbol_values_gradients, None), thetas_gradients
 
-#         # Connect symbol values gradients to QNN variables
-#         with tf.GradientTape() as phis_tape:
-#           symbol_values = total_circuit.symbol_values
-#           tiled_symbol_values = tf.tile(
-#               tf.expand_dims(symbol_values, 0), [tf.shape(idx)[0], 1])
-#         phis_gradients = phis_tape.gradient(
-#             tiled_symbol_values,
-#             variables,
-#             output_gradients=symbol_values_gradients,
-#             unconnected_gradients=tf.UnconnectedGradients.ZERO)
+      return forward_pass, grad_fn
 
-#         thetas_gradients = thetas_tape.gradient(
-#             forward_pass,
-#             variables,
-#             output_gradients=upstream[0],
-#             unconnected_gradients=tf.UnconnectedGradients.ZERO)
+    return _inner_expectation()
 
-#         # Note: upstream gradient is already a coefficient in tg and pg.
-#         variables_gradients = [
-#             tg + pg for tg, pg in zip(thetas_gradients, phis_gradients)
-#         ]
-#         return tuple(), variables_gradients
+  def _expectation(self, circuits, symbol_names, symbol_values, observables):
+    if isinstance(observables, tf.Tensor):
+      ops = observables
+      post_process = lambda x: x
+    elif isinstance(observables.energy, energy.PauliMixin):
+      ops = observables.operator_shards
+      post_process = lambda y: tf.map_fn(
+          lambda x: tf.expand_dims(
+              observables.energy.operator_expectation(x), 0), y)
+    else:
+      return self._sampled_expectation(circuits, symbol_names, symbol_values, observables)
 
-#       return forward_pass, grad_fn
-
-#     return _inner_expectation()
-
-#   def _expectation(self, circuits, symbol_names, symbol_values, observables):
-#     if isinstance(observables, tf.Tensor):
-#       total_circuit = self.circuit
-#       ops = observables
-#       post_process = lambda x: x
-#     elif isinstance(observables.energy, energy.PauliMixin):
-#       total_circuit = self.circuit + observables.circuit_dagger
-#       ops = observables.operator_shards
-#       post_process = lambda y: tf.map_fn(
-#           lambda x: tf.expand_dims(
-#               observables.energy.operator_expectation(x), 0), y)
-#     else:
-#       return self._sampled_expectation(initial_states, observables)
-
-#     num_circuits = tf.shape(circuits)[0]
-#     num_ops = tf.shape(ops)[0]
-#     tiled_ops = tf.tile(tf.expand_dims(ops, 0), [num_circuits, 1])
-#     repetitions = tf.tile(tf.expand_dims(self._expectation_samples, 1), [num_circuits, num_ops])
-#     expectations = self._expectation_layer(
-#       circuits,
-#       symbol_names=symbol_names,
-#       symbol_values=symbol_values,
-#       operators=tiled_ops,
-#       repetitions=repetitions)
-#     return post_process(expectations)
+    num_circuits = tf.shape(circuits)[0]
+    num_ops = tf.shape(ops)[0]
+    tiled_ops = tf.tile(tf.expand_dims(ops, 0), [num_circuits, 1])
+    repetitions = tf.tile(tf.expand_dims(self._expectation_samples, 1), [num_circuits, num_ops])
+    expectations = self._expectation_layer(
+      circuits,
+      symbol_names=symbol_names,
+      symbol_values=symbol_values,
+      operators=tiled_ops,
+      repetitions=repetitions)
+    return post_process(expectations)
