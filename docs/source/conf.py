cb4279acb58196748f4b4fbdc29b648d560eb6a5
zaqqwerty <antonio.martinez.8128@gmail.com>
2022-04-27 00:21:04 -0500

    generate docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
+
 # sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath("../qhbmlib"))
 
 
 # -- Project information -----------------------------------------------------
@@ -31,6 +33,11 @@ release = '0.3'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  'myst_parser',
+  'sphinx.ext.mathjax',
+  'sphinx.ext.viewcode',
+  'sphinx.ext.autosummary',
+  'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -41,13 +48,19 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# Add .rst for the index .md for use with myst-parser
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
---
+++ docs/source/qhbmlib.rst
@@ -0,0 +1,31 @@
+qhbmlib package
+===============
+
+Subpackages
+-----------
+
+.. toctree::
+   :maxdepth: 4
+
+   qhbmlib.data
+   qhbmlib.inference
+   qhbmlib.models
+
+Submodules
+----------
+
+qhbmlib.utils module
+--------------------
+
+.. automodule:: qhbmlib.utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: qhbmlib
+   :members:
+   :undoc-members:
+   :show-inheritance:
